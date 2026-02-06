2f0a293fb39a1f2be0a2c899c934c2172cc8b55a
mathiucemarcel-ui <mathiucemarcel@gmail.com>
2026-02-06 12:01:24 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,18 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Preencha todos os campos.");
+    return;
+  }
+
+  // Simulação de login (MVP)
+  localStorage.setItem("axisUser", email);
+
+  // Redireciona
+  window.location.href = "dashboard.html";
+});
 
